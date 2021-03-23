a14fe21e8a2bd6ddb407912ff82668dbb70cc484
Umar Sharief <umar.sharief04@gmail.com>
2021-03-23 06:45:04 +0000

    Added primes quiz and changed readme accordingly.

--- SwitchMode.py
+++ SwitchMode.py
@@ -1,6 +1,7 @@
 def SwitchMode():
   switch = input('Do you want to switch modes?\n')
   if switch.upper() == 'YES':
-    from main import start
+    Start()
+    
     
     
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-start = input('Hello. This is a tool to find more about numbers. What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\n')
+#Go to README.md for guidance on how to use this program. 
 from PrimeorComposite import *
 from SquareorCube import *
-from SwitchMode import SwitchMode
+from SwitchMode import *
+import random
+level = 1
+start = input('Hello. This is a tool to find more about numbers. What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\nType 5 for a quiz about primes and composites.\nGo to code, then go to README.md for guidance and information.') 
 while start == '1': 
   number = input('Enter a number.') 
   print(number, PrimeorComposite(int(number)))
@@ -32,20 +35,48 @@ while start =='4':
   squares = 0
   cubenums = 'The cube numbers between ', num1,'and',num2,'are:'
   squarenums = 'The square numbers between', num1, 'and',num2,'are:'
-  for l in range (num1,num2+1):
-    if SquareorCube(l)=='is both a square number and a cube number.':
+  for l in range (num1,num2+1,4):
+    if SquareorCube(l)=='is a power of 6, so it is both a square number and a cube number.':
       squares = squares + 1
+      squarenums = squares,l
       cubes = cubes + 1 
-      squarenums = squarenums,str(l)
-      cubenums = cubenums,str(l) 
+      squarenums = squarenums,l
+      cubenums = cubenums,l
   for l in range (num1,num2+1):
     if SquareorCube(l)=='is a cube number.':
       cubes = cubes + 1
-      cubenums = cubenums,str(l)
+      cubenums = cubenums,l
   for l in range (num1,num2+1):
     if SquareorCube(l)=='is a square number.':
       squares = squares + 1
-      squarenums = squarenums,str(l)  
-  print('There are',cubes,'cube numbers, and',squares,'square numbers between',num1, 'and',num2,'.')
-  print(cubenums,'\n',squarenums)
+      squarenums = squarenums,l  
+  total = ('There are ',str(cubes),' cube numbers, and ',str(squares),' square numbers between ',str(num1), ' and ',str(num2),'.')
+  print(''.join(total))
+  list = str(cubenums),'\n',str(squarenums)
+  print(''.join(list))
   SwitchMode()
+while start == '5':
+  score = 0
+  for a in range(10):
+    rand = random.randint(level*50-49,level*50)
+    text = 'Is ',str(rand),' a prime number?\n'
+    answer = input(''.join(text))
+    if PrimeorComposite(rand)=='is a prime number.':
+      if  answer.upper()=='YES':
+        print('Well done!')
+        score = score +1 
+      else:
+        print('Oops. Incorrect answer!')
+    else:
+      if answer.upper()=='NO':
+        print('Well done!')
+        score = score + 1
+      else:
+        print('Oops. Incorrect answer!')
+  if score>9:
+    level = level + 1
+    levelup = 'Level up! You are now on level ',str(level),'! Good work! You will now get harder questions!'
+    print(''.join(levelup))
+    
+
+
