e401d6ae2857a1fbf8d516d0b37a1106a2aae55e
Umar Sharief <>
2021-04-25 13:06:53 +0000

    300+ lines of code! I added customisable optional typewriter effect! Also I (think I) fixed getting the same number multiple times in quizzes.

--- README.md
+++ README.md
@@ -6,10 +6,56 @@ Hello there and welcome to my Maths stuff!
 This program currently identifies primes, composites, squares and cubes from within a range of number/s that you specify.   
 To get started, just type a number from 1-4 to choose one of the options.   
 All inputs must be written in integers (whole numbers). For the switching modes option, it allows you to switch modes if you type 'yes' in lowercase, uppercase or a mix, otherwise it continues on the same mode.
-When finding squares/cubes/primes/composites in a certain range, the first number you will be asked for will be the **start** of the range, and the second number the **end** of the range, ***including itself***.  
+## Primes/squares/cubes in range calculator
+When finding squares/cubes/primes/composites in a certain range, the first number you will be asked for will be the **start** of the range, and the second number the **end** of the range, ***including itself***.
+## Quiz  
 For the square/cube quiz, type *square* if you think it is square, *cube* if you think it is cube, *both* if you think it is both square and cube, and *no* if you think it is neither square nor cube.
+## How questions are picked
+The numbers picked quadratically increase depending on the level. There are theoretically infinite levels, but when the level gets really high the game becomes slower. If you really want to cheat your way to high levels, fork this program, click UserAccounts, then click your nickname, then click PrimeLevel or SquareOrCubeLevel depending on what you want to change, then change the number to what you want. *I won't be changing anyone's levels here!* ((-:)
+### How the range of numbers is chosen
+The formula to choose a number in each level is:
+> maximum = level squared times ten
+> minimum = maximum of previous level. 
+
+There is an exception for level 1, because the formula above wouldn't work, so it is set manually to 1-10. Example question ranges include:
+> level 1 = 1-10
+level 2 = 10-40
+level 5 = 160-250
+level 10 = 810-1000
+level 20 = 3610-4000
+level 50 = 2401-2500
+level 100 = 9801-10000
+level 200 = 39601-40000
+level 500 = 249001-250000
+
+### How each individual question is chosen
+1. Primes/squares and cubes respectively are counted within the range of numbers (which depends on level)
+2. Each individual number is checked to see if it is prime/square or cube
+3. The chance of that number getting picked is one divided by the amount of primes/composites or squares+cubes/not square or cube.
+#### Example 1
+> Level:2
+Quiz:primes
+Range:10-40
+8 Primes,22 composites between 10-40
+Number currently being checked:17
+17 is prime.
+1 divided by 8 primes=12.5% chance of getting 17,or any prime in level 2 range.
+
+#### Example 2
+> Level:3
+Quiz:squares+cubes
+Range:40-90
+3 squares/cubes,47 other numbers between 10-40
+Number currently being checked:82
+82 is not square or cube.
+1 divided by 47 not square or cube=about 2.13% chance of getting 47, or any other number that isn't square or cube
+
+## Typewriter Effect!
+When choosing typewriter effect, the speed actually increases with a lower number because it is measured in ***average*** **seconds between each letter**.
+So if you type in 0.1, 0.1 is multiplied by 2 to make 0.2, and the delay between each letter is a random number between 0 and 0.2 seconds. So it averages out to 0.1 seconds. 
+The reason I made it like this instead of a fixed delay is so that it feels more natural, because you never type each letter with the exact same speed.
+
 Hope you like this tool! Have a good day!
-![Screenshot demonstrating program](https://www.bing.com/images/blob?bcid=RADoODU78ogClg "program in action!")
-[Video demonstrating program](https://www.loom.com/share/e7a3212ec2af446c81699a7f9109135f)
+
 To use this tool on replit, click [here](https://replit.com/@idkwhatnamelol/Umars-maths-stuff?v=1)  
-## Known issues
+
--- main.py
+++ main.py
@@ -9,13 +9,21 @@ global SquareorCubeNumber
 global SquareorCubeRange
 global PrimeQuiz
 global SquareorCubeQuiz
+global typewriter
+global speed
+global chooseTP
+from time import sleep
+import sys
 def PrimeorCompositeNumber():
-  number = input('Enter a number.')
+  typewriter('Enter a number.\n')
+  number=int(input())
   print(number, PrimeorComposite(int(number)))
   SwitchMode()
 def PrimeorCompositeRange():
-    num1 = int(input('Enter the first number\n'))
-    num2 = int(input('Enter the second number\n'))
+    typewriter('Enter the first number\n')
+    num1=int(input())
+    typewriter('Enter the second number\n')
+    num2=int(input())
     amount = 0
     primes = ('these are the prime numbers between' + str(num1) + ' and ' +
                       str(num2) + ': ')
@@ -24,19 +32,22 @@ def PrimeorCompositeRange():
             amount = amount + 1
             primes = primes + str(l) + ', '
     if amount == 1:
-        print('there is 1 prime number between', num1, 'and', num2)
+        typewriter('there is 1 prime number between', num1, 'and', num2)
     else:
-        print('there are', amount, 'prime numbers between', num1, 'and', num2)
+        typewriter('there are', amount, 'prime numbers between', num1, 'and', num2)
     Total = ''.join(primes)
-    print(Total)
+    typewriter(Total)
     SwitchMode()
 def SquareorCubeNumber():
-    number = int(input('Enter a number to find whether it is squared or cubed!\n'))
+    typewriter('Enter a number to find whether it is squared or cubed!\n')
+    number=int(input())
     print(number, SquareorCube(number))
     SwitchMode()
 def SquareorCubeRange():
-  num1 = int(input('Enter the first number\n'))
-  num2 = int(input('Enter the second number.\n'))
+  typewriter('Enter the first number\n')
+  num1=int(input())
+  typewriter('Enter the second number.\n')
+  num2=int(input())
   cubes = 0
   squares = 0
   cubenums = 'The cube numbers between ' + str(num1) + ' and ' + str(
@@ -59,11 +70,10 @@ def SquareorCubeRange():
     if SquareorCube(l) == 'is a square number.':
       squares = squares + 1
       squarenums = squarenums + str(l) + ', '
-  total = ('There are ', str(cubes), ' cube numbers, and ', str(squares),
-                   ' square numbers between ', str(num1), ' and ', str(num2), '.')
-  print(''.join(total))
+  total = ('There are ', str(cubes), ' cube numbers, and ', str(squares),' square numbers between ', str(num1), ' and ', str(num2), '.')
+  typewriter(''.join(total))
   totallist = cubenums + '\n' + squarenums
-  print(''.join(totallist))
+  typewriter(''.join(totallist))
   SwitchMode()
 def PrimeQuiz():
   score = 0
@@ -75,10 +85,13 @@ def PrimeQuiz():
   else:
       highestPL=int(readPL)
       PLs=list(range(1,int(highestPL)+1))
-      choosePL=int(input('Type a level from 1 to '+str(highestPL)+'\n'))
+      typewriter('Type a level from 1 to '+str(highestPL)+'\n')
+      choosePL=int(input())
       while PLs.count(choosePL)==0:
-          choosePL=int(input('Invalid level, please try again\n'))
+          typewriter('Invalid level, please try again\n')
+          choosePL=input()
       primelevel=choosePL
+  chosen=''
   for a in range(10):
       primes=0
       composites=0
@@ -88,44 +101,51 @@ def PrimeQuiz():
             primeorcomposite=random.randint(1,3)
             if primeorcomposite==3:
               for l in range(1,10):
+                if not chosen.__contains__(l):
                   if PrimeorComposite(l)=='is a prime number.':
-                      rand=random.randint
-                      if rand==4:
-                          randnum=l
+                    rand=random.randint(1,4)
+                    if rand==4:
+                      chosen=chosen+str(l)
+                      randnum=l
                   elif l%2 > 0 or l==2:
-                      rand=random.randint(1,6)
-                      if rand==6:
-                          randnum=l
+                    rand=random.randint(1,6)
+                    if rand==6:
+                      chosen=chosen+str(l)
+                      randnum=l
           else:
+            if not chosen.__contains__(l):
               for l in range((primelevel-1)**2*10+1,primelevel**2*10):
-                  if PrimeorComposite(l)=='is a prime number.':
-                      primes=primes+1
-                  else:
-                      composites=composites=composites+1
+                if PrimeorComposite(l)=='is a prime number.':
+                  primes=primes+1
+                else:
+                  composites=composites=composites+1
               for l in range((primelevel-1)**2*10+1,primelevel**2*10):
-                  if PrimeorComposite(l)=='is a prime number':
-                      rand=random.randint(1,primes)
-                      if rand==primes:
-                          randnum=l
-                  elif l%2 > 0 or l == 2:
-                      rand=random.randint(1,composites)
-                      if rand==composites:
-                          randnum=l
+                if PrimeorComposite(l)=='is a prime number':
+                  rand=random.randint(1,primes)
+                  if rand==primes:
+                    chosen=chosen+str(l)
+                    randnum=l
+                elif l%2 > 0 or l == 2:
+                    rand=random.randint(1,composites)
+                    if rand==composites:
+                      chosen=chosen+str(l)
+                      randnum=l
       text = 'Is ', str(randnum), ' a prime number?\n'
-      answer = input(''.join(text))
+      typewriter(''.join(text))
+      answer=input()
       if PrimeorComposite(randnum) == 'is a prime number.':
           correct = 'yes'
       else:
           correct = 'no'
       if answer.lower()==correct:
           score=score+1
-          print('Well done!')
+          typewriter('Well done!')
       else:
-          print('Oops! Correct answer is: '+correct)
+          typewriter('Oops! Correct answer is: '+correct)
   if score > 9 and primelevel==highestPL:
     highestPL = highestPL + 1
     primelevelup = 'Level up! You are now on level ', str(highestPL), '! Good work! You will now get harder questions!'
-    print(''.join(primelevelup))
+    typewriter(''.join(primelevelup))
     with open('UserAccounts/'+nickname+'/PrimeLevel.md','w') as PLfile:
         PLfile.write(str(highestPL))
   SwitchMode()
@@ -134,6 +154,7 @@ def SquareorCubeQuiz():
     score=0
     nonpowers=()
     powers=()
+    chosen=()
     with open('UserAccounts/'+nickname+'/SquareOrCubeLevel.md') as SOCLfile:
         readSOCL = SOCLfile.read()
         if readSOCL =='':
@@ -142,44 +163,53 @@ def SquareorCubeQuiz():
         else:
             highestSOCL=int(readSOCL)
             SOCLs=list(range(1,int(highestSOCL)+1))
-            chooseSOCL=int(input('Choose a level from 1 to '+str(highestSOCL)+'\n'))
+            typewriter('Choose a level from 1 to '+str(highestSOCL)+'\n')
+            chooseSOCL=int(input())
             while SOCLs.count(chooseSOCL)==0:
-                chooseSOCL=int(input('Invalid level, please try again\n'))
+                typewriter('Invalid level, please try again\n')
+                chooseSOCL=input()
             squareorcubelevel=chooseSOCL
     for a in range(10):
         powers=0
         nonpowers=0
         randnum=()
         while randnum==():
-            if squareorcubelevel == 1:
-                powerornonpower=random.randint(1,3)
-                if powerornonpower==3:
-                  for l in range(10):
-                    if SquareorCube(l)=='is a prime number.':
-                      rand=random.randint(1,4)
-                      if rand==4:
-                        randnum=l
-                    else:
-                        rand=random.randint(1,6)
-                        if rand==6:
-                            randnum=l
-            else:
-                for l in range((squareorcubelevel-1)**2*10+1,squareorcubelevel**2*10):
-                  if SquareorCube(l)=='is not a square or cube number.':
-                      nonpowers=nonpowers+1
+          if squareorcubelevel == 1:
+            powerornonpower=random.randint(1,3)
+            if powerornonpower==3:
+              for l in range(10):
+                if not chosen.__contains__(l):
+                  if SquareorCube(l)=='is a prime number.':
+                    rand=random.randint(1,4)
+                    if rand==4:
+                      chosen=chosen,l
+                      randnum=l
                   else:
-                      powers=powers+1
-                for l in range((squareorcubelevel-1)**2*10+1,squareorcubelevel**2*10):
-                      if SquareorCube(l)=='is not a square or cube number.':
-                          rand=random.randint(1,nonpowers)
-                          if rand==nonpowers:
-                              randnum=l
-                      else:
-                          rand=random.randint(1,powers)
-                          if rand==powers:
-                              randnum=l
+                    rand=random.randint(1,6)
+                    if rand==6:
+                      randnum=l
+                      chosen=chosen,l
+            else:
+              for l in range((squareorcubelevel-1)**2*10+1,squareorcubelevel**2*10):
+                if SquareorCube(l)=='is not a square or cube number.':
+                  nonpowers=nonpowers+1
+                else:
+                  powers=powers+1
+              for l in range((squareorcubelevel-1)**2*10+1,squareorcubelevel**2*10):
+                if not chosen.__contains__(l):
+                  if SquareorCube(l)=='is not a square or cube number.':
+                    rand=random.randint(1,nonpowers)
+                    if rand==nonpowers:
+                      randnum=l
+                      chosen=chosen,l
+                    else:
+                      rand=random.randint(1,powers)
+                      if rand==powers:
+                        randnum=l
+                        chosen=chosen,l
         text = 'Is '+ str(randnum)+ " a square number or cube number? Type 'both', 'square', 'cube' or 'no' accordingly. \n"
-        answer = input(''.join(text))
+        typewriter(text)
+        answer = input()
         if SquareorCube(randnum) == 'is a power of 6, so it is both a square number and a cube number.':
             correct='both'
         elif SquareorCube(randnum) == 'is a square number.':
@@ -189,17 +219,22 @@ def SquareorCubeQuiz():
         else:
             correct='no'
         if answer.lower()==correct:
-            print('Well done!')
+            typewriter('Well done!')
             score=score+1
         else:
-            print('Oops! The correct answer is:'+correct+'.')
+            typewriter('Oops! The correct answer is:'+correct+'.')
     if score > 9 and squareorcubelevel==highestSOCL:
         highestSOCL=highestSOCL+1
         SOCLup = 'Level up! You are now on level ' + str(highestSOCL) + '. Good job!'
-        print(''.join(SOCLup))
+        print(typewriter(''.join(SOCLup)))
         with open('UserAccounts/'+nickname+'/SquareOrCubeLevel.md','w') as SOCLfile:
             SOCLfile.write(str(highestSOCL))
     SwitchMode()
+def typewriter(t):
+  for x in t:
+    print(x, end='')
+    sys.stdout.flush()
+    sleep(random.uniform(0,speed))
 nickname =input('Hello! Enter a nickname to save progress.\nIf you have used this tool before, enter the nickname you used last.\n')
 if not os.path.exists('UserAccounts/'+nickname):
     os.mkdir('UserAccounts/'+nickname)
@@ -207,15 +242,39 @@ SOCLfile = open('UserAccounts/'+nickname+'/SquareOrCubeLevel.md','a+')
 SOCLfile.close()
 PLfile=open('UserAccounts/'+nickname+'/PrimeLevel.md','a+')
 PLfile.close()
+createTSfile=open('UserAccounts/'+nickname+'/TypeSpeed.md','a+')
+createTSfile.close()
+with open('UserAccounts/'+nickname+'/TypeSpeed.md','r+')as typespeed:
+  TSread=typespeed.read() 
+if TSread=='':
+  write=input('Do you want to turn on typewriter effects?')
+  if write.lower()=='yes':
+    speed=float(input('Type a number to change speed of typewriter effect.\nNote that a lower amount means a higher speed, because it is measured  in average seconds delay between each letter.\nPress code, or go to\nhttps://github.com/noneofyourbusiness1415252/Maths-stuff#readme\nfor more information.\n'))
+    typewriter('This is how fast the effect will be. Are you happy with your changes?\n')
+    happy=input()
+    while happy.lower()=='no':
+      typewriter('Type a number to change speed.\n')
+      speed=input()
+      typewriter('This is how fast the effect will be. Are you happy with your changes?\n')
+      happy=input()
+  else:
+    speed=0
+  with open('UserAccounts/'+nickname+'/TypeSpeed.md','w')as typespeed:
+    typespeed.write(str(speed*2))
+else:
+  speed=float(TSread)
 global SwitchMode
 def SwitchMode():
   global start
   if start =='':
-    start = input('Hello, '+nickname+'. This is a tool to find more about numbers. What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\nType 5 for a quiz about primes and composites.\nType 6 for a quiz about squares and cubes.\nPlease go to code, then go to README.md for guidance and information.\n Please report any bugs or feedback in the comments below, or on\nhttps://github.com/noneofyourbusiness1415252/Maths-stuff/issues\n')
+    typewriter('Hello, '+nickname+'. This is a tool to find more about numbers. What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\nType 5 for a quiz about primes and composites.\nType 6 for a quiz about squares and cubes.\nType 7 to change typewriter effect speed!\nPlease click code, or visit\nhttps://github.com/noneofyourbusiness1415252/Maths-stuff#readme\nfor more information.\nPlease report any bugs or feedback in the comments below, or on\nhttps://github.com/noneofyourbusiness1415252/Maths-stuff/issues\n')
+    start=input()
   else:
-    switch = input('Do you want to switch modes?\n')
+    typewriter('Do you want to switch modes?\n')
+    switch=input()
     if switch.upper()=='YES':
-      start=input('What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\nType 5 for a quiz about primes and composites.\nType 6 for a quiz about squares and cubes.\n')
+      typewriter('What do you want to do? Type in a number to go to one of these modes: \nType 1 to find out if a number is prime or composite. \nType 2 to find out how many Prime numbers are between 2 numbers\nType 3 to find out if a number is squared or cubed.\nType 4 to find out how many squares and cubes there are between 2 numbers.\nType 5 for a quiz about primes and composites.\nType 6 for a quiz about squares and cubes.\n')
+      start=input()
   if start=='1':
     PrimeorCompositeNumber()
   elif start=='2':
@@ -228,8 +287,23 @@ def SwitchMode():
     PrimeQuiz()
   elif start=='6':
     SquareorCubeQuiz()
+  elif start=='7':
+    typewriter('Do you want to turn on typewriter effects?')
+    write=input()
+    if write.lower()=='yes':
+      typewriter('Type a number to change speed of typewriter effect.\nNote that a lower amount means a higher speed, because it is measured  in average seconds delay between each letter.\n')
+      speed=float(input())
+      typewriter('This is how fast the effect will be. Are you happy with your changes?\n')
+      happy=input()
+      while happy.lower()!='yes':
+        speed=float(input('Type a number to change speed.\n'))
+        typewriter('This is how fast the effect will be.Are you happy with your changes?\n')
+        happy=input()
+      with open('UserAccounts/'+nickname+'/TypeSpeed.md','w')as TSFile:
+        TSFile.write(str(speed))
+      SwitchMode()
   else:
-    start=input('Invalid option. Please type a number from 1 to 6 to choose a mode.')
+    start=input('Invalid option. Please type a number from 1 to 7 to choose a mode.\n')
 SwitchMode()
 
 
